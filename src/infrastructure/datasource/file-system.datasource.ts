4b7cd105bd9b89a6e5a707989ee5786ebf323cb8
Raul Ochoa <raulochoa039@gmail.com>
2024-02-15 12:10:19 -0600

    Crear primer datasource: FS

---
+++ src/infrastructure/datasource/file-system.datasource.ts
@@ -0,0 +1,74 @@
+import fs from 'fs';
+
+import { LogDataSource } from "../../domain/datasources/log.datasource";
+import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
+
+export class FileSystemDatasource implements LogDataSource {
+    private readonly logPath = 'logs/';
+    private readonly allLogPath = 'logs/logs-all.log';
+    private readonly mediumLogPath = 'logs/logs-medium.log';
+    private readonly highLogPath = 'logs/logs-high.log';
+
+    constructor() { 
+        this.createLogFiles();
+    }
+
+    private createLogFiles = () => {
+        if( !fs.existsSync(this.logPath) ){
+            fs.mkdirSync(this.logPath);
+        }
+
+        // Nos ahorra tener que escribir la condicion 3 veces.
+        [
+            this.allLogPath,
+            this.mediumLogPath,
+            this.highLogPath,
+        ].forEach( path => {
+            if( fs.existsSync(path) ) return;
+
+            fs.writeFileSync(path, '');
+        })
+    }
+
+    private getLogsFromFile = (path: string): LogEntity[] => {
+        const content = fs.readFileSync(path, 'utf-8');
+        
+        const logs = content.split('\n').map(
+            log => LogEntity.fromJson(log)
+        );
+
+        return logs;
+    }
+
+    async saveLog(newLog: LogEntity): Promise<void> {
+        const logAsJSON = `${ JSON.stringify(newLog) }\n`;
+        
+        fs.appendFileSync(this.allLogPath, logAsJSON);
+
+        if(newLog.level === LogSeverityLevel.low) return;
+
+        if(newLog.level === LogSeverityLevel.medium) {
+            fs.appendFileSync(this.mediumLogPath, logAsJSON);
+        }
+        else{
+            fs.appendFileSync(this.highLogPath, logAsJSON);
+        }
+    }
+
+    async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+        switch(severityLevel){
+            case LogSeverityLevel.low:
+                return this.getLogsFromFile(this.allLogPath);
+
+            case LogSeverityLevel.medium:
+                return this.getLogsFromFile(this.mediumLogPath);
+
+            case LogSeverityLevel.high:
+                return this.getLogsFromFile(this.highLogPath);
+
+            default:
+                throw new Error(`${ severityLevel } not implemented.`);
+        }
+    }
+    
+}
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,12 @@
 import { CheckService } from "../domain/use-cases/checks/check-service.use-case";
+import { FileSystemDatasource } from "../infrastructure/datasource/file-system.datasource";
+import { LogRepositoryImplementation } from "../infrastructure/repositories/log.repository";
 import { CronService } from "./cron/cron-service";
+
+// Instancias de repositorios.
+const fileSystemLogRepository = new LogRepositoryImplementation( 
+    new FileSystemDatasource() 
+);
 
 export class Server {
     constructor() { }
@@ -10,9 +17,11 @@ export class Server {
         CronService.createJob(
             '*/5 * * * * *',
             () => {
-                let url = 'https://google.com';
+                // let url = 'https://google.com';
+                let url = 'http://localhost:3000';
 
                 new CheckService(
+                    fileSystemLogRepository,
                     () => console.log(`${url} is ok`),
                     ( error ) => console.log( error ),
                 ).execute(url);
